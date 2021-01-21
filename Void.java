e67dc6408ba61ecbe51f33942074162c3788ed0c
ardhanurfajri <ardha_nur_29rpl@student.smktelkom-mlg.sch.id>
2021-01-21 19:21:17 +0700

    Main dan Void Method

---
+++ Void.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Ardha Nur Fajri
+ */
+public class Void {
+    int a=9;
+    void percabangan(){
+        if (a==9){
+           System.out.println("Y");
+        }else{
+            System.out.println("N");
+        }
+    }
+    void perulangan(){
+        for(a=9;a<=10;a++){
+            System.out.println("X");
+        }
+    }
+}
---
+++ main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+/**
+ *
+ * @author Ardha Nur Fajri
+ */
+public class main {
+    public static void main(String[]args){
+        Void iya=new Void();
+        iya.perulangan();
+        iya.percabangan();
+    }
+}
